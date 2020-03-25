df76d02a675ad0d8e6413563c813652f3cc27ea0
maisa12 <nikamaisa@gmail.com>
2020-03-25 21:40:35 +0400

    adding  ads, validation ads and categories

--- query.js
+++ query.js
@@ -1,41 +1,85 @@
 const ad = require('./models/ad');
 const category = require('./models/category')
 const user = require('./models/user')
-let createAd = async function(name, category, description, picture, price){
+let createAd = async function(name, author, category, description, picture, price, active){
          await ad.create({
             name: name,
-            author: "admin",
+            author: author,
             category: category,
             description: description,
             picture: picture,
-            price: price
+            price: price,
+            active: active
         })
+const checkCreation = await ad.findAll({where:{
+    name: name,
+    author: author,
+    category: category,
+    description: description,
+    picture: picture,
+    price: price,
+    active: active
+}, raw: true, attributes: ['id']}); 
+   if(checkCreation.length!==0){
+       return true
+   }
+   else{
+      return false
+   }
 }
 let createCategory = async function(name, position, slug){
+    const checkPosition = await category.findAll({where:{position: position}, raw: true, attributes: ['id']});
+    const checkSlug = await category.findAll({where:{slug: slug}, raw: true, attributes: ['id']});
+    if(checkPosition.length!==0 && checkSlug.length!==0){
+        return "both"
+    }
+    if(checkPosition.length!==0){
+        return "position"
+    }
+    if(checkSlug.length!==0){
+        return "slug"
+    }
     await category.create({
        name: name,
        position: position,
        slug: slug,
    })
+   return "success"
 }
 let createUser = async function(name, email, phone, status, password){
-   
     const checkEmail = await user.findAll({where:{email: email}, raw: true, attributes: ['id']});
+    console.log(checkEmail.length) 
     if(checkEmail.length===0){
-    await user.create({
+   const newuser = await user.create({
         name_lastname: name,
         email: email,
         phone_number: phone,
         status: status,
         password: password
-   })
-   return true
+   });
+     const checkCreation = await user.findAll({where:{email: email}, raw: true, attributes: ['id']}); 
+   if(checkCreation.length!==0){
+       return true
+   }
+   else{
+      return false
+   }
 }
 else{
     return false
 }
-
 }
+let categoryArr = async()=>{
+   const categoryArray = await category.findAll({raw: true, attributes: ['name', 'position', 'slug', 'id']});
+   return categoryArray
+}
+let authorArr = async()=>{
+    const authorArray = await user.findAll({raw: true, attributes: ['name_lastname', 'id']});
+    return authorArray
+ }
+
 module.exports.createAd = createAd;
 module.exports.createCategory = createCategory;
 module.exports.createUser = createUser;
+module.exports.categoryArr = categoryArr;
+module.exports.authorArr = authorArr;
--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const {createCategory} = require('./query');
-const {createUser} = require('./query');
+const {createUser, categoryArr, authorArr, createCategory, createAd} = require('./query');
 const port = process.env.PORT || 8000;
 app.use(cors());
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 app.post('/add/category', jsonParser, async(req,res)=>{
-  await createCategory(req.body.name, req.body.position, req.body.slug)
-    res.send("success")
-    console.log(req.body)
+  const response = await createCategory(req.body.name, req.body.position, req.body.slug);
+    res.send(response)
+    
   })
 app.post('/add/user', jsonParser, async(req,res)=>{
     const {name, lastname, email, status, phoneNumber, password} = req.body;
-   const resp = await createUser(name+lastname, email, phoneNumber, status, password);
-      res.send(resp)
+   const response = await createUser(name+"%"+lastname, email, phoneNumber, status, password);
+      res.send(response);
    })
+   app.post('/add/ad', jsonParser, async(req,res)=>{
+    const {name, author, category, description, picture, price, active} = req.body;
+   const response = await createAd(name, author, category, description, picture, price, active);
+      res.send(response);
+   })
+   app.get('/category', jsonParser, async(req,res)=>{
+   const resp = await categoryArr();
+      res.send(JSON.stringify(resp));
+   })
+   app.get('/authors', jsonParser, async(req,res)=>{
+    const resp = await authorArr();
+       res.send(JSON.stringify(resp));
+    })
+   
 app.listen(port, ()=>console.log(`Server is listening: ${port}`))
